e3dfac3ab785a2a795037a68ada54afd86a4cd42
jannyHou <juehou@ca.ibm.com>
2018-02-23 18:22:05 -0500

    feat: add keys.ts

--- packages/openapi-v3/src/keys.ts
+++ packages/openapi-v3/src/keys.ts
@@ -1,12 +1,12 @@
 // Copyright IBM Corp. 2018. All Rights Reserved.
-// Node module: @loopback/openapi-v2
+// Node module: @loopback/openapi-v3
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-export namespace OAI2Keys {
-  export const METHODS_KEY = 'openapi-v2:methods';
-  export const METHODS_WITH_PARAMETERS_KEY = 'openapi-v2:methods:parameters';
-  export const PARAMETERS_KEY = 'openapi-v2:parameters';
-  export const CLASS_KEY = 'openapi-v2:class';
-  export const CONTROLLER_SPEC_KEY = 'openapi-v2:controller-spec';
+export namespace OAI3Keys {
+  export const METHODS_KEY = 'openapi-v3:methods';
+  export const METHODS_WITH_PARAMETERS_KEY = 'openapi-v3:methods:parameters';
+  export const PARAMETERS_KEY = 'openapi-v3:parameters';
+  export const CLASS_KEY = 'openapi-v3:class';
+  export const CONTROLLER_SPEC_KEY = 'openapi-v3:controller-spec';
 }
