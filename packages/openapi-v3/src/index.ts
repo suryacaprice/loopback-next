2238945b906cde4a7200352d660056bc4e90d733
jannyHou <juehou@ca.ibm.com>
2018-02-23 15:57:45 -0500

    feat: copy openapi-v2 for compare

---
+++ packages/openapi-v3/src/index.ts
@@ -0,0 +1,10 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/openapi-v2
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+export * from './api-decorator';
+export * from './controller-spec';
+export * from './json-to-schema';
+export * from './parameter-decorator';
+export * from './operation-decorator';
